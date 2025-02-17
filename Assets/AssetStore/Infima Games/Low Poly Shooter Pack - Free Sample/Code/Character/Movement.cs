6698fbf1538127929f01f372575ad2b93c742f7d
Mohamed Osama <engmohamedosama@yandex.com>
2025-02-17 19:39:35 +0200

    game enviroment, enemy mode and animations. using navmesh

---
+++ Assets/AssetStore/Infima Games/Low Poly Shooter Pack - Free Sample/Code/Character/Movement.cs
@@ -0,0 +1,200 @@
+﻿using System.Linq;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace InfimaGames.LowPolyShooterPack
+{
+    [RequireComponent(typeof(NavMeshAgent), typeof(CapsuleCollider))]
+    public class Movement : MovementBehaviour
+    {
+        #region FIELDS SERIALIZED
+
+        [Header("Audio Clips")]
+        
+        [Tooltip("The audio clip that is played while walking.")]
+        [SerializeField]
+        private AudioClip audioClipWalking;
+
+        [Tooltip("The audio clip that is played while running.")]
+        [SerializeField]
+        private AudioClip audioClipRunning;
+
+        [Header("Speeds")]
+
+        [SerializeField]
+        private float speedWalking = 5.0f;
+
+        [Tooltip("How fast the player moves while running."), SerializeField]
+        private float speedRunning = 9.0f;
+
+        #endregion
+
+        #region FIELDS
+        
+        /// <summary>
+        /// Attached NavMeshAgent.
+        /// </summary>
+        private NavMeshAgent navMeshAgent;
+        /// <summary>
+        /// Attached CapsuleCollider.
+        /// </summary>
+        private CapsuleCollider capsule;
+        /// <summary>
+        /// Attached AudioSource.
+        /// </summary>
+        private AudioSource audioSource;
+        
+        /// <summary>
+        /// True if the character is currently grounded.
+        /// </summary>
+        private bool grounded;
+
+        /// <summary>
+        /// Player Character.
+        /// </summary>
+        private CharacterBehaviour playerCharacter;
+        /// <summary>
+        /// The player character's equipped weapon.
+        /// </summary>
+        private WeaponBehaviour equippedWeapon;
+        
+        /// <summary>
+        /// Array of RaycastHits used for ground checking.
+        /// </summary>
+        private readonly RaycastHit[] groundHits = new RaycastHit[8];
+
+        #endregion
+
+        #region UNITY FUNCTIONS
+
+        /// <summary>
+        /// Awake.
+        /// </summary>
+        protected override void Awake()
+        {
+            //Get Player Character.
+            playerCharacter = ServiceLocator.Current.Get<IGameModeService>().GetPlayerCharacter();
+        }
+
+        /// Initializes the FpsController on start.
+        protected override void Start()
+        {
+            //NavMeshAgent Setup.
+            navMeshAgent = GetComponent<NavMeshAgent>();
+            navMeshAgent.updateRotation = false;
+            navMeshAgent.updatePosition = true;
+
+            //Cache the CapsuleCollider.
+            capsule = GetComponent<CapsuleCollider>();
+
+            //Audio Source Setup.
+            audioSource = GetComponent<AudioSource>();
+            audioSource.clip = audioClipWalking;
+            audioSource.loop = true;
+        }
+
+        /// Checks if the character is on the ground.
+        private void OnCollisionStay()
+        {
+            //Bounds.
+            Bounds bounds = capsule.bounds;
+            //Extents.
+            Vector3 extents = bounds.extents;
+            //Radius.
+            float radius = extents.x - 0.01f;
+            
+            //Cast. This checks whether there is indeed ground, or not.
+            Physics.SphereCastNonAlloc(bounds.center, radius, Vector3.down,
+                groundHits, extents.y - radius * 0.5f, ~0, QueryTriggerInteraction.Ignore);
+            
+            //We can ignore the rest if we don't have any proper hits.
+            if (!groundHits.Any(hit => hit.collider != null && hit.collider != capsule)) 
+                return;
+            
+            //Store RaycastHits.
+            for (var i = 0; i < groundHits.Length; i++)
+                groundHits[i] = new RaycastHit();
+
+            //Set grounded. Now we know for sure that we're grounded.
+            grounded = true;
+        }
+			
+        protected override void FixedUpdate()
+        {
+            //Move.
+            MoveCharacter();
+            
+            //Unground.
+            grounded = false;
+        }
+
+        /// Moves the camera to the character, processes jumping and plays sounds every frame.
+        protected override void Update()
+        {
+            //Get the equipped weapon!
+            equippedWeapon = playerCharacter.GetInventory().GetEquipped();
+            
+            //Play Sounds!
+            PlayFootstepSounds();
+
+            // Rotate the character based on movement direction
+            Vector2 frameInput = playerCharacter.GetInputMovement();
+            Vector3 direction = new Vector3(frameInput.x, 0.0f, frameInput.y);
+            if (direction != Vector3.zero)
+            {
+                //Quaternion targetRotation = Quaternion.LookRotation(direction);
+               // transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 10f);
+            }
+        }
+
+        #endregion
+
+        #region METHODS
+
+        private void MoveCharacter()
+        {
+            #region Calculate Movement Velocity
+
+            //Get Movement Input!
+            Vector2 frameInput = playerCharacter.GetInputMovement();
+            //Calculate local-space direction by using the player's input.
+            var movement = new Vector3(frameInput.x, 0.0f, frameInput.y).normalized;
+            
+            float speed = playerCharacter.IsRunning() ? speedRunning : speedWalking;
+
+            //World space velocity calculation. This allows us to move the NavMeshAgent.
+            movement = transform.TransformDirection(movement);
+
+            //Update NavMeshAgent Destination
+            if (movement.magnitude > 0)
+            {
+                Vector3 targetPosition = transform.position + (movement * Time.deltaTime * speed);
+                //navMeshAgent.SetDestination(targetPosition);
+                navMeshAgent.transform.position = targetPosition;
+            }
+
+            #endregion
+        }
+
+        /// <summary>
+        /// Plays Footstep Sounds. This code is slightly old, so may not be great, but it functions alright-y!
+        /// </summary>
+        private void PlayFootstepSounds()
+        {
+            //Check if we're moving on the ground. We don't need footsteps in the air.
+            if (grounded && navMeshAgent.velocity.sqrMagnitude > 0.1f)
+            {
+                //Select the correct audio clip to play.
+                audioSource.clip = playerCharacter.IsRunning() ? audioClipRunning : audioClipWalking;
+                //Play it!
+                if (!audioSource.isPlaying)
+                    audioSource.Play();
+            }
+            //Pause it if we're doing something like flying, or not moving!
+            else if (audioSource.isPlaying)
+                audioSource.Pause();
+        }
+
+        #endregion
+    }
+}
